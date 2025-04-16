e7009012f9f0962916ab94b362ed704b84005499
Aravinth_Siva <68836256+AravinthS75@users.noreply.github.com>
2025-04-16 18:57:35 +0000

    added env files

---
+++ angularapp-frontend/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://psychic-spork-7ww59r94q67cr6jv-8081.app.github.dev/',
+    featureToggle: true,
+    appVersion: '1.0.0-dev',
+  };
+  
