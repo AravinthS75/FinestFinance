0bad639e7d2d265b3c50245c845a919042dede15
Aravinth_Siva <68836256+AravinthS75@users.noreply.github.com>
2025-04-17 15:33:47 +0000

    added changes to loanrepo

--- src/main/java/com/example/springapp/repository/LoanRepository.java
+++ src/main/java/com/example/springapp/repository/LoanRepository.java
@@ -1,6 +1,8 @@
 package com.example.springapp.repository;
 
 import java.util.List;
+import java.util.Optional;
+
 import com.example.springapp.model.Loan;
 
 import org.springframework.data.jpa.repository.JpaRepository;
